a9ee6302daf5d9e2f0f4b1c80db9d95931baa539
AqordeoN <aqordivika@yahoo.co.id>
2023-09-21 20:39:15 +0800

    Tailwind: add color native, add font Jost

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-	<div class="bg-slate-100 flex w-screen h-screen items-center justify-center gap-x-4">
+
+	<!-- <div class="bg-slate-100 flex w-screen h-screen items-center justify-center gap-x-4">
 		<h1 class="text-gray-400">This page is blank</h1>
 		<NuxtLink to="/materi">Materi</NuxtLink>
 		<NuxtLink to="/materi/Lulus Ujian Nasional/2">Quiz 2</NuxtLink>
-	</div>
+	</div> -->
 </template>
 
 <script setup>
