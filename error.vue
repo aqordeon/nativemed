7fd78f62265e212bae8b360c2a287099e4f6524f
AqordeoN <aqordivika@yahoo.co.id>
2023-09-28 11:06:07 +0800

    Page: error

--- error.vue
+++ error.vue
@@ -1,24 +1,41 @@
 <template>
-  <div class="w-screen h-screen grid justify-center items-center bg-red-500">
-    <div class="grid items-center justify-center gap-y-4 px-4">
-      <h2 class="text-white font-semibold text-2xl">This is an Error Page 404 :)</h2>
-      <div class="place-self-center bg-gray-100 text-slate-800 w-fit shadow rounded-full py-2 px-6 cursor-pointer hover:bg-gray-200" @click="goBack">Go Back</div>
+    <div class="flex items-center justify-center w-full h-full pt-28">
+        <div class="text-indigo-600">
+            <div class="container max-w-7xl grid place-items-center mb-4" :class="{ block: error.statusCode == 404 }">
+                <div class="px-5 text-center">
+                    <h2 class="text-2xl text-indigo-400">{{ error.statusCode }}</h2>
+                </div>
+            </div>
+            <div class="container max-w-7xl grid place-items-center"
+                :class="{ hidden: error.statusCode != 404 }">
+                <div class="px-6 md:px-5 text-center max-w-5xl">
+                    <img format="webp" src="@/assets/images/disconnect.svg" alt=""
+                        class="md:mx-auto w-96 px-14 md:px-0 mb-6" />
+                    <h3 class="font-bold text-3xl mb-2">
+                        Page not available :)
+                    </h3>
+                    <p class="mb-4">
+                        We are still here to help you around.
+                    </p>
+                </div>
+                <button @click="() => router.back()" class="flex justify-center bg-indigo-700 hover:bg-indigo-500 text-gray-100 px-4 py-1.5 rounded-md border border-indigo-600">
+                    Go back
+                </button>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
+
 defineProps({
-  error: Object,
-})
+    error: Object,
+});
 
 useHead({
-  title: "404 - Page not Found",
-})
+    title: "404 - Page not Found",
+});
 
-const router = useRouter()
+const router = useRouter();
 
-const goBack = () => {
-  router.back()
-}
 </script>
