a8a23efd5151c9db93fa93a4dc5b300af33ce2ab
AqordeoN <aqordivika@yahoo.co.id>
2023-09-30 15:22:25 +0800

    Responsive: Navbar mobile

--- components/Layouts/Navbar.vue
+++ components/Layouts/Navbar.vue
@@ -1,26 +1,41 @@
 <template>
 	<section class="w-screen flex justify-between items-center py-3 px-4 lg:px-6">
-			<div class="flex items-center cursor-pointer">
-				<img src="@/assets/images/Native.png" alt="" class="w-10 lg:w-14">
-				<span class="lg:text-xl xl:text-2xl font-bold uppercase text-[#5271a8]">Native Satria Nusantara</span>
+		<div class="flex items-center cursor-pointer">
+			<img src="@/assets/images/Native.png" alt="" class="w-10 lg:w-14">
+			<span class="lg:text-xl xl:text-2xl font-bold uppercase text-[#5271a8]">Native Satria Nusantara</span>
+		</div>
+
+		<!-- Navbar: Menu -->
+		<div class="hidden md:grid grid-cols-3 justify-between gap-x-5 lg:gap-x-8 xl:gap-x-11 place-content-center">
+			<NuxtLink :to="navbar.url" v-for="(navbar, index) in navbarList" :key="index"
+				class="group relative text-center font-medium">
+				<p class="px-2 lg:px-4"
+					:class="[route.path.includes(navbar.url.slice(0, -1).toLowerCase()) ? 'text-native-600' : 'text-native-300 hover:text-native-500']">
+					{{ navbar.name }}</p>
+				<div :class="[route.path.includes(navbar.url.slice(0, -1).toLowerCase()) ? 'bg-native-600 w-full left-0' : 'bg-native-300 w-0 left-1/2 group-hover:w-1/2 group-hover:left-[25%]']"
+					class="absolute -bottom-2 h-0.5 rounded-full transition-all duration-200 ease-in-out" />
+			</NuxtLink>
+		</div>
+
+		<!-- Navbar: Register/Sign in -->
+		<div class="hidden md:flex gap-x-3 lg:gap-x-6">
+			<NuxtLink to="/register" class="group relative flex px-2 items-center cursor-pointer">
+				<p>Register</p>
+				<div :class="[route.path.includes('/register') ? 'bg-native-600 w-full left-0' : 'bg-native-300 w-0 left-1/2 group-hover:w-1/2 group-hover:left-[25%]']"
+					class="absolute -bottom-0 h-0.5 bg-native-600 rounded-full transition-all duration-200 ease-in-out" />
+			</NuxtLink>
+			<div class="px-3 py-2.5 rounded-md bg-native-600 text-white cursor-pointer hover:bg-native-500">Sign in</div>
+		</div>
+		
+		<!-- Hamburger -->
+		<div class="md:hidden">
+			<div class="bg-native-200 px-3 py-1 cursor-pointer" @click="isHamburgerOpen = !isHamburgerOpen">+</div>
+			<div v-if="isHamburgerOpen" class="z-30 bg-white/70 left-0 bottom-0 fixed w-full h-[93%]" />
+			<div class="z-40 fixed bottom-0 bg-red-500 h-[93%] w-4/6 transition-all duration-200 ease-in-out" :class="[isHamburgerOpen ? 'right-0' : '-right-full']">
+
 			</div>
 
-			<!-- Navbar: Menu -->
-			<div class="grid grid-cols-3 justify-between gap-x-5 lg:gap-x-8 xl:gap-x-11 place-content-center">
-				<NuxtLink :to="navbar.url" v-for="(navbar, index) in navbarList" :key="index" class="group relative text-center font-medium">
-					<p class="px-2 lg:px-4" :class="[route.path.includes(navbar.url.slice(0, -1).toLowerCase()) ? 'text-native-600' : 'text-native-300 hover:text-native-500']">{{ navbar.name }}</p>
-					<div :class="[route.path.includes(navbar.url.slice(0, -1).toLowerCase()) ? 'bg-native-600 w-full left-0' : 'bg-native-300 w-0 left-1/2 group-hover:w-1/2 group-hover:left-[25%]']" class="absolute -bottom-2 h-0.5 rounded-full transition-all duration-200 ease-in-out" />
-				</NuxtLink>
-			</div>
-
-			<!-- Navbar: Register/Sign in -->
-			<div class="flex gap-x-3 lg:gap-x-6">
-				<NuxtLink to="/register" class="group relative flex px-2 items-center cursor-pointer">
-					<p>Register</p>
-					<div :class="[route.path.includes('/register') ? 'bg-native-600 w-full left-0' : 'bg-native-300 w-0 left-1/2 group-hover:w-1/2 group-hover:left-[25%]']" class="absolute -bottom-0 h-0.5 bg-native-600 rounded-full transition-all duration-200 ease-in-out" />
-				</NuxtLink>
-				<div class="px-3 py-2.5 rounded-md bg-native-600 text-white cursor-pointer hover:bg-native-500">Sign in</div>
-			</div>
+		</div>
 		<!-- <div class="bg-indigo-500 flex gap-x-2">
 			<NuxtLink to="/">Index</NuxtLink>
 			<NuxtLink v-if="!authenticated" to="/login">Login</NuxtLink>
@@ -44,6 +59,8 @@ const logout = () => {
 	router.push("/login")
 }
 
+const isHamburgerOpen = ref(false)
+
 const navbarList = [
 	{
 		name: 'Home',
@@ -60,5 +77,3 @@ const navbarList = [
 ]
 
 </script>
-
-<style lang="scss"></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,8 @@
 	<Title>Native Med</Title>
 	<div class="text-native-600 overflow-hidden">
 		<CommonsDebugComp v-if="isDebug" />
-		<LayoutsNavbar class="relative z-10" />
-		<main class="">
+		<LayoutsNavbar class="fixed top-0 left-0 z-20 md:relative" />
+		<main class="mt-16 md:mt-0 isolate">
 			<slot />
 		</main>
 		<LayoutsFooterBar />
