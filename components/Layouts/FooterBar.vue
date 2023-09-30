41b657cfcaefd49172709f30d4cbfbd9949a3e06
AqordeoN <aqordivika@yahoo.co.id>
2023-09-30 16:14:48 +0800

    Responsive: Footer mobile

--- components/Layouts/FooterBar.vue
+++ components/Layouts/FooterBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <section class="bg-native-100 pl-12 py-9 flex gap-x-12 xl:gap-x-20">
-        <div class="flex items-center">
+    <section class="bg-native-100 text-center md:text-left md:pl-12 py-4 md:py-9 flex gap-x-12 xl:gap-x-20">
+        <div class="hidden md:flex items-center">
             <img src="@imgs/Native.png" alt="Native logo" class="w-32 aspect-square">
         </div>
-        <div class="flex gap-x-24 xl:gap-x-52">
-            <div class="flex flex-col gap-y-5 xl:gap-y-9 justify-between">
-                <div class="space-y-2.5">
+        <div class="w-full md:w-fit flex gap-x-24 xl:gap-x-52">
+            <div class="w-full md:w-fit flex flex-col gap-y-5 xl:gap-y-9 justify-between">
+                <div class="hidden md:inline space-y-2.5">
                     <p class="font-bold">info@nativemed.id</p>
                     <!-- <p class="font-light flex flex-col">
                         <span>Lorem ipsum dolor sit amet consectetur</span>
@@ -16,7 +16,7 @@
                 </div>
                 <p class="text-sm">&copy; 2023 <span class="font-semibold">Native Satria Indonesia</span> All Right Reserved.</p>
             </div>
-            <div>
+            <div class="hidden md:block">
                 <p class="font-bold">Perusahaan</p>
                 <p class="font-light flex flex-col">
                     <NuxtLink class="hover:underline" to="/aboutus">Tentang Kami</NuxtLink>
@@ -24,7 +24,7 @@
                     <!-- <NuxtLink to="#" class="whitespace-nowrap">Masukkan untuk produk</NuxtLink> -->
                 </p>
             </div>
-            <div>
+            <div class="hidden md:block">
                 <p class="font-bold">Temukan Kami</p>
                 <div class="flex gap-x-2.5">
                     <NuxtLink to="#"><img src="@/assets/images/socmed/instagram.png" alt="" class="w-5 aspect-square"></NuxtLink>
