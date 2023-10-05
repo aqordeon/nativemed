325c4b57de053d94b0c6322a7b62799c9a6d65a6
AqordeoN <aqordivika@yahoo.co.id>
2023-10-05 21:20:58 +0800

    UI: Soal, Penilaian Soal

---
+++ pages/materi/[materiName]/[quizid]/tryout/[nomorSoal].vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="w-full h-full bg-native-50">
+        <div class="w-full h-full bg-native-100 px-12 py-4 flex flex-col gap-y-8">
+            <span class="text-gray-500 font-bold text-xs ">Materi Lorem Ipsum / Quiz Lorem Ipsum / Pengerjaan Soal</span>
+            <p class="font-bold text-2xl block text-gray-600">⬅️ Materi: {{ useRouter().currentRoute.value.params.materiName }}</p>
+        </div>
+        <div class="">
+            <div class="bg-white rounded-md text-gray-700 py-2.5 px-6 flex w-fit gap-x-2.5 mx-auto -translate-y-1/2">
+                <div class="rounded-md bg-native-300 w-8 aspect-square flex items-center justify-center">01</div>
+                <span>:</span>
+                <div class="rounded-md bg-native-300 w-8 aspect-square flex items-center justify-center">01</div>
+                <span>:</span>
+                <div class="rounded-md bg-native-300 w-8 aspect-square flex items-center justify-center">01</div>
+            </div>
+        </div>
+        <div class="px-12 pb -5 space-y-5">
+            <div class="flex gap-x-2.5">
+                <div class="bg-white text-gray-600 px-7 py-3 flex justify-between items-center w-full">
+                    <span>Soal 1</span>
+                    <div class="bg-slate-300 text-gray-500 py-2.5 px-5 rounded-full flex gap-x-5 ">
+                        <span>1 / 50</span>
+                        <span>⬅️➡️</span>
+                    </div>
+                </div>
+                <div class="bg-white border border-native-500 rounded-md flex items-center px-4 gap-x-2.5">
+                    <span>📃</span>
+                    <span class="whitespace-nowrap">Lihat Jawaban Saya</span>
+                </div> 
+                <div class="bg-native-600 px-10 text-white whitespace-nowrap flex items-center rounded-md">
+                    Kumpulkan Quiz
+                </div>
+            </div>
+
+            <div class="bg-white text-gray-700 py-7 px-8 text-lg flex gap-x-2.5">
+                <!-- Soal -->
+                <div class="bg-native-50 rounded-md px-6 py-5 w-full">
+                    <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <img src="@imgs/soal.png" alt="" class="h-56">
+                    <div class="flex flex-col gap-y-2.5">
+                        <div v-for="(pilihan, index) in pilihanGanda" :key="index" class="group cursor-pointer flex rounded-md hover:text-white py-5 px-6 bg-white hover:bg-native-500 gap-x-6">
+                            <div class="w-8"><span class="h-8 aspect-square flex items-center justify-center rounded-full border border-gray-600 group-hover:border-white">A</span></div>
+                            <p>{{ pilihan }}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- List Soal -->
+                <div class="bg-native-50 rounded-md py-4 px-4 w-[80%] space-y-5">
+                    <div class="text-xl whitespace-nowrap">List Soal</div>
+                    <div class="flex flex-col gap-y-2.5">
+                        <div v-for="(soal, index) in listSoal" :key="index" class="bg-white rounded-md space-x-2.5 py-2 px-5 flex">
+                            <span>{{ index + 1 }}.</span>
+                            <p class="truncate w-full whitespace-nowrap">{{ soal }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-around">
+                <div class="bg-white border border-native-600 text-native-600 w-fit px-7 py-5 rounded-md text-sm">⬅️ Soal sebelumnya</div>
+                <div class="bg-white border border-native-600 w-fit px-7 py-5 rounded-md text-sm">Ragu-ragu</div>
+                <div class="bg-amber-500 w-fit px-7 py-5 rounded-md text-sm">Soal sebelumnya  ➡️</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: "tryout",
+    // middleware: 'auth',
+})
+
+const pilihanGanda = [
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+]
+
+const listSoal = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ipsum dolor sit amet consectetur adipisicing elit. Delectus, quos.',
+]
+</script>
---
+++ pages/materi/[materiName]/[quizid]/tryout/hasilakhir.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="w-full h-full bg-native-50">
+        <div class="w-full h-full bg-native-100 px-12 py-4 flex flex-col gap-y-8">
+            <span class="text-gray -500 font-bold text-xs ">Materi Lorem Ipsum / Quiz Lorem Ipsum / Pengerjaan Soal</span>
+            <p class="font-bold text-2xl block text-gray-600 mb-4">⬅️ Materi: {{ useRouter().currentRoute.value.params.materiName }}</p>
+        </div>
+        <div class="px-12 pt-5 pb-7 space-y-5">
+            <div class="bg-white text-gray-600 px-7 py-12 flex flex-col justify-center items-center gap-y-10">
+                <div class="font-bold text-3xl">Penilaian Soal</div>
+                <div class="text-native-600 text-xl">Selamat Anda Lulus!✨</div>
+                <div class="aspect-square w-48 rounded-full bg-gray-100 flex items-center justify-center">
+                    <div class="flex items-center flex-col gap-y-4">
+                        <div class="text-4xl text-native-600 font-bold">80</div>
+                        <div class="text-2xl leading-none">Nilai Anda</div>
+                    </div>
+
+                </div>
+                <div>
+                    <div class="text-white grid grid-cols-3 w-fit gap-x-5">
+                        <div class="space-y-2.5">
+                            <div class="bg-lime-500 w-full text-center px-5 py-4 rounded-md text-sm">Soal yang benar</div>
+                            <div class="text-lime-500 flex items-center justify-center font-bold text-3xl py-7 border border-lime-500 rounded-md">40</div>
+                        </div>
+                        <div class="space-y-2.5">
+                            <div class="bg-rose-500 w-full text-center px-5 py-4 rounded-md text-sm">Soal yang salah</div>
+                            <div class="text-rose-500 flex items-center justify-center font-bold text-3xl py-7 border border-rose-500 rounded-md">40</div>
+                        </div>
+                        <div class="space-y-2.5">
+                            <div class="bg-amber-500 w-fit px-5 py-4 rounded-md text-sm">Soal yang tidak dijawab</div>
+                            <div class="text-amber-500 flex items-center justify-center font-bold text-3xl py-7 border border-amber-500 rounded-md">40</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-native-600 w-fit mx-auto px-7 py-5 rounded-md text-white text-sm">📃Pembahasan</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: "tryout",
+    // middleware: 'auth',
+})
+</script>
